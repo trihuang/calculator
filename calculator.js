2020b911fee7df0e00ccb9dd437053c6e40153fb
trihuang <124452877+trihuang@users.noreply.github.com>
2023-02-19 13:31:54 +0800

    Implement calculator logic for the operators

--- calculator.js
+++ calculator.js
@@ -7,23 +7,27 @@ const operatorButtons = document.querySelectorAll('.operator');
 const display = document.querySelector('.display');
 
 let displayValue = "";
+let operandOne = null;
+let operatorOne = null;
+let operatorTwo = null;
+let computation = null;
 
 allClearButton.addEventListener('click', allClear);
 clearButton.addEventListener('click', clear);
 decimalButton.addEventListener('click', addDecimal);
-equalsButton.addEventListener('click', displayResult);
+equalsButton.addEventListener('click', equals);
 numButtonsEventListeners(numButtons);
 operatorsEventListeners(operatorButtons);
 
 function operate(operator, num1, num2) {
     if (operator == "plus") {
-        add(num1, num2);
+        return add(num1, num2);
     } else if (operator == "minus") {
-        substract(num1, num2);
+        return subtract(num1, num2);
     } else if (operator == "multiply") {
-        multiply(num1, num2);
+        return multiply(num1, num2);
     } else if (operator == "divide") {
-        divide(num1, num2);
+        return divide(num1, num2);
     }
 }
 
@@ -40,31 +44,112 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return Math.round((num1 / num2) * 100) / 100;
+    if (num2 == 0) {
+        return "ERROR";
+    } else return Math.round((num1 / num2) * 100) / 100;
 }
 
 function allClear() {
     display.value = "";
     displayValue = "";
+    operandOne = null;
+    operatorOne = null;
+    operatorTwo = null;
+    computation = null;
+    //isValidOperatorInput = false;
 }
 
 function clear() {
-    displayValue = displayValue.substring(0, displayValue.length - 1);
-    display.value = displayValue;
+    /*
+    if ((operatorOne != null) && operatorTwo == null && !isValidOperatorInput) {
+        operatorOne = null;
+        isValidOperatorInput = true;
+    } else if (operatorTwo != null && !isValidOperatorInput) {
+        operatorTwo = null;
+        isValidOperatorInput = true;
+    } else {
+        displayValue = displayValue.toString().substring(0, displayValue.length - 1);
+        display.value = displayValue;
+    } 
+    */
 }
 
-function addDecimal() {
-    console.log('decimal');
+function addDecimal(e) {
+    if (displayValue === 'ERROR') return;
+    let decimal = e.target.textContent;
+    if (displayValue === "") {
+        displayValue = '0';
+        displayValue += decimal;
+        display.value = displayValue;
+    } else if (!displayValue.includes(decimal)) {
+        displayValue += decimal;
+        display.value = displayValue;
+    }
 }
 
-function displayResult() {
-    console.log('result');
+function equals() {
+    if (displayValue === 'ERROR') return;
+    if (operatorOne === null) return;
+    else if (operatorTwo !== null) {
+        computation = operate(operatorTwo, Number(operandOne), Number(displayValue));
+        if (computation === 'ERROR') {
+            displayValue = computation;
+            display.value = displayValue;
+        } else {
+            displayValue = computation;
+            display.value = displayValue;
+            operandOne = displayValue;
+            operatorOne = null;
+            operatorTwo = null;
+        }
+    } else {
+        computation = operate(operatorOne, Number(operandOne), Number(displayValue));
+        if (computation === 'ERROR') {
+            displayValue = computation;
+            display.value = displayValue;
+        } else {
+            displayValue = computation;
+            display.value = displayValue;
+            operandOne = displayValue;
+            operatorOne = null;
+            operatorTwo = null;
+        }
+    }
 }
 
 function displayNum(e) {
-    if (displayValue.length < 11) {
-        displayValue += e.target.textContent;
+    if (displayValue === 'ERROR') return;
+    let input = e.target.textContent;
+    if (displayValue === '0' && input !== 0) {
+        displayValue = input;
         display.value = displayValue;
+    } else if (displayValue.length < 10) {
+        displayValue += input;
+        display.value = displayValue;}
+        //isValidOperatorInput = true;
+}
+
+function handleOperator(e) {
+    if (displayValue === 'ERROR') return;
+    let nextOperator = e.target.id;
+    if (operatorOne !== null && operatorTwo === null) {
+        operatorTwo = nextOperator;
+        computation = operate(operatorOne, Number(operandOne), Number(displayValue));
+        displayValue = computation;
+        display.value = displayValue;
+        operandOne = displayValue;
+        displayValue = "";
+    } else if (operatorOne !== null && operatorTwo !== null) {
+        computation = operate(operatorTwo, Number(operandOne), Number(displayValue));
+        operatorTwo = nextOperator;
+        displayValue = computation;
+        display.value = displayValue;
+        operandOne = displayValue;
+        displayValue = "";
+    } else {
+        operatorOne = nextOperator;
+        operandOne = displayValue;
+        displayValue = "";
     }
 }
 
@@ -76,7 +161,7 @@ function numButtonsEventListeners(nodes) {
 
 function operatorsEventListeners(nodes) {
     for (let i = 0; i < nodes.length; i++) {
-        nodes[i].addEventListener('click', displayResult);
+        nodes[i].addEventListener('click', handleOperator);
     }
 }
 
